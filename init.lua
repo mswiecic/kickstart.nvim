8cab83db9df6b8eab2204e694f144803d36d964a
Majkel <majkel@pop-os.localdomain>
2024-09-14 19:49:45 +0200

    Autosave

--- init.lua
+++ init.lua
@@ -928,6 +928,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.init',
+  require 'custom.plugins.autosave',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
